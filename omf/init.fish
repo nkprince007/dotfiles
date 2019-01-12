d3d0515ee23030d45ac332e698abc23b06faa6c3
Naveen Kumar Sangi <nkprince007@users.noreply.github.com>
2019-01-12 19:36:16 +0530

    init.fish: Unalias nvim

--- omf/init.fish
+++ omf/init.fish
@@ -3,7 +3,6 @@ fenv source $HOME/.profile
 # misc
 alias brewtree='brew graph --installed | dot -Tpng -ograph.png'
 alias dc=docker-compose
-alias vim=nvim
 alias dk=docker
 
 # additional functions
@@ -15,9 +14,12 @@ function docker-stop
     killall Docker
 end
 
+function fish_title
+    true
+end
+
 # GnuPG preparation
 if test -f ~/.gnupg/.gpg-agent-info
     fenv source ~/.gnupg/.gpg-agent-info
     export GPG_AGENT_INFO
 end
-
